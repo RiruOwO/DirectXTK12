6d8282ca1d712340439c5843be9d5519eb3b4b0b
RiruOwO <vrcsub0304@gmail.com>
2026-06-11 09:44:15 +0900

    テスト

--- MainProject/Scenes/MainScene.cpp
+++ MainProject/Scenes/MainScene.cpp
@@ -147,7 +147,7 @@ NextScene MainScene::Update(const float deltaTime)
 	bg_position2_.x -= 5.0f;
 	if (bg_position2_.x <= -2560.0f)
 		bg_position2_.x += 2560.0f * 2.0f;
-
+	
 	return NextScene::Continue;
 }
 
